437581644e9acd3f11f9e3271e29e3ed42d88ec7
Gautam Jain <gautamjain@Gautams-MacBook-Air.local>
2024-01-15 03:53:37 +0530

    font-weight in tailwind config, setting up vercel config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+      },
+      fontWeight: {
+        'light': 300,
+        'regular': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 800,
+      },
+    },
   },
   plugins: [],
 }
