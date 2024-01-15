ea21d2899b847ed7a5cae239a020411a01080490
Gautam Jain <gautamjain@Gautams-MacBook-Air.local>
2024-01-16 01:15:36 +0530

    cookie fix for get api

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,11 +1,13 @@
 // src/axiosConfig.js
 import axios from 'axios';
+const cookieValue = 'abuse_interstitial=64a1-34-125-28-20.ngrok-free.app';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://hello-world-empty-bread-c761.gautamjain673.workers.dev',
+  baseURL: 'http://64a1-34-125-28-20.ngrok-free.app/',
   timeout: 5000, // Set a timeout (milliseconds) for requests
   headers: {
     'Content-Type': 'application/json',
+    'cookie':cookieValue
     // You can add common headers here
   },
 });
